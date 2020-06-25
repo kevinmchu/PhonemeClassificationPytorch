9f72ca00bcb9d62e84d34dbf14b31bcf493a1d4c
Collins Lab <collinslab@gmail.com>
2020-06-24 20:34:30 -0400

    replace scaler.pickle

--- main.py
+++ main.py
@@ -338,9 +338,9 @@ if __name__ == '__main__':
     test_feat_list = "data/test.txt"
 
     # Parameters
-    model_type = "LSTM"
+    model_type = "MLP"
     model_idx = 0
-    label_type = "moa"
+    label_type = "phoneme"
     num_valid_utts = 184
 
     # Read in feature list files
