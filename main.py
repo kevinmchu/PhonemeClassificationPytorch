fef3e19a61e488993498810a2956e86c1b01ae59
Collins Lab <collinslab@gmail.com>
2020-07-26 20:35:42 -0400

    add hidden layer back in to cnn

--- main.py
+++ main.py
@@ -300,7 +300,7 @@ def train_and_validate(conf_file, num_models):
 
 if __name__ == '__main__':
     # User inputs
-    conf_file = "conf/LSTM_rev_mspec.txt"
+    conf_file = "conf/CNN_rev_mspec.txt"
     num_models = 1
 
     # Train and validate
