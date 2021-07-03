45fa89bcb126634203a8c8a721b1fc3fdf80c5d5
kevinmchu <kmc100@duke.edu>
2021-07-03 19:37:06 -0400

    add exception to keep trying to read from feature file in case of disconnection from server

--- file_loader.py
+++ file_loader.py
@@ -126,28 +126,20 @@ def read_feat_file(filename, conf_dict):
         X (np.array): matrix of features
         y (list): list of phoneme labels
     """
-    # Load npz file containing features and phones
-    data = np.load(filename.replace(".txt", ".npz"))
+    # If disconnected from server, keep trying to read from file
+    io_flag = True
+    while io_flag:
+        filename = filename.replace(".txt", ".npz")
+        try:
+            data = np.load(filename)
+            io_flag = False
+        except OSError:
+            print("File not found: " + filename + " . Attempting to read again.")
+            continue
+
     X = data["feats"]
     y = data["phones"]
     
-    # If disconnected, keep trying to read from file
-    #io_flag = True
-    #while io_flag:
-    #    try:
-    #        # Read in features and labels as a string
-    #        file_obj = open(filename, "r")
-    #        x = file_obj.readlines()
-    #        file_obj.close()
-    #        io_flag = False
-    #    except:
-    #        continue
-    
-    # Extract features and labels
-    #featsAndLabs = list(map(lambda a: a.split(), x))
-    #X = np.array(list(map(lambda a: a[0:conf_dict["num_coeffs"]], featsAndLabs)), dtype='float32')
-    #y = list(map(lambda a: a[-1], featsAndLabs))
-
     # Train on speech enhanced using irm, if applicable
     if "train_condition" in conf_dict.keys():
         if conf_dict["train_condition"] == "irm":
